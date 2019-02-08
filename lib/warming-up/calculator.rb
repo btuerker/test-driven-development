06a485b76a34cc57cf9271fc9a8d9c56370cc306
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-02-08 19:01:00 +0300

    add more methods and test cases

--- lib/warming-up/calculator.rb
+++ lib/warming-up/calculator.rb
@@ -1,9 +1,25 @@
 class Calculator
-  def add num1, num2
-    num1 + num2
+  def add num1, num2, *nums
+    result = num1 + num2
+    nums.each { |e| result += e }
+    result
   end
 
-  def subs num1, num2
-    num1 - num2
+  def substract num1, num2, *nums
+    result = num1 - num2
+    nums.each { |e| result -= e }
+    result
+  end
+
+  def multiply num1, num2, *nums
+    result = num1 * num2
+    nums.each { |e| result *= e }
+    result
+  end
+
+  def divide num1, num2, *nums
+    result = num1 / num2
+    nums.each { |e| result /= e }
+    result
   end
 end
--- spec/warming-up/calculator_spec.rb
+++ spec/warming-up/calculator_spec.rb
@@ -2,16 +2,50 @@ require("./lib/warming-up/calculator")
 
 describe Calculator do
   describe "#add" do
-    it "returns the sum of two numbers" do
+    it "should returns the sum of two numbers" do
       calculator = Calculator.new
       expect(calculator.add(5, 2)).to eql(7)
     end
+
+    it "should returns the sum of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.add(5, 2, 7)).to eql(14)
+    end
   end
 
-  describe "#subs" do
-    it "returns the substract of two numbers" do
+  describe "#substract" do
+    it "should returns the substract of two numbers" do
       calculator = Calculator.new
-      expect(calculator.subs(5,2)).to eql(3)
+      expect(calculator.substract(5,2)).to eql(3)
+    end
+
+    it "should returns the substract of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.substract(5,2,3)).to eql(0)
+    end
+  end
+
+  describe "#multiply" do
+    it "should returns multiply of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(5,2)).to eql(10)
+    end
+
+    it "should returns multiply of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(5,2,3)).to eql(30)
+    end
+  end
+
+  describe "#divide" do
+    it "should returns divide of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.divide(10,5)).to eql(2)
+    end
+
+    it "should returns divide of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.divide(10,5,2)).to eql(1)
     end
   end
 end
